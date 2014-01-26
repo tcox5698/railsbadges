7ec011b71c6b6b592a7ea78d7963bd46f5947cf2
tcox5698 <tcox56_98@yahoo.com>
2014-01-25 18:59:32 -0600

    admin can list users

--- features/support/custom_env.rb
+++ features/support/custom_env.rb
@@ -9,9 +9,17 @@ end
 Capybara.javascript_driver = :selenium_chrome
 
 begin
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :truncation, {:except => %w[roles]}
 rescue NameError
     raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
 load "#{Rails.root}/db/seeds.rb"
+
+Before do | scenario |
+  unless User.exists? email: 'superuser@meritbadges.com'
+    superuser = User.create email: 'superuser@meritbadges.com', confirmed_at: Time.now, password: 'password', password_confirmation: 'password'
+
+    superuser.roles << Role.find_by_name('superuser')
+  end
+end
