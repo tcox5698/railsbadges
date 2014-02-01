5487f83d3fc4df41a9cdc2d4e265447b021696c9
tcox5698 <tcox56_98@yahoo.com>
2014-02-01 09:46:11 -0600

    got different superuser cases rspecced

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,11 +5,14 @@ class UserSessionsController < Devise::SessionsController
   def create
     super
 
-    if self.resource.email.equal? 'superuser@meritbadges.com'
-      flash[:alert] = 'MeritBadges is not initialized.  Please configure another user as superuser.'
-    end
-
-    if Role.find_by_name('superuser').users.count < 2
+    if self.resource.email == 'superuser@meritbadges.com'
+      if Role.find_by_name('superuser').users.count < 2
+        flash[:alert] = 'MeritBadges is not initialized.  Please configure another user as superuser.'
+      else
+        sign_out
+        flash[:alert] = 'Default superuser is disabled.  Please login as a real person.'
+      end
+    elsif Role.find_by_name('superuser').users.count < 2
       sign_out
       flash[:alert] = 'MeritBadges is not initialized.  Please login as superuser and configure another user as superuser.'
     end
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -2,12 +2,48 @@ require 'spec_helper'
 
 describe UserSessionsController do
   describe 'POST #create' do
-    describe 'when logging in as normal user' do
-      let(:current_user) { create :user, email: 'newguy@here.com' }
 
-      before do
-        @request.env["devise.mapping"] = Devise.mappings[:user]
+    before do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+    end
+
+    describe 'when logging in as superuser' do
+      describe 'when alternative superuser has been already been configured' do
+        before do
+          alt_super = create :user
+          alt_super.roles << Role.find_by_name('superuser')
+          post :create, user: {email: 'superuser@meritbadges.com', password: 'password'}
+        end
+
+        it { should redirect_to(root_path) }
+
+        describe 'flash alert' do
+          subject { flash[:alert] }
+          it { should eq 'Default superuser is disabled.  Please login as a real person.' }
+        end
+
+        its(:current_user) { should be_nil }
       end
+
+      describe 'when alternative superuser has NOT been already been configured' do
+        before do
+          post :create, user: {email: 'superuser@meritbadges.com', password: 'password'}
+        end
+
+        it { should redirect_to(root_path) }
+
+        describe 'flash alert' do
+          subject { flash[:alert] }
+          it { should eq 'MeritBadges is not initialized.  Please configure another user as superuser.' }
+        end
+
+        its(:current_user) { should eq User.find_by_email('superuser@meritbadges.com') }
+      end
+
+    end
+
+    describe 'when logging in as non-super user' do
+      let(:current_user) { create :user, email: 'newguy@here.com' }
 
       describe 'when alternative superuser has been already been configured' do
         before do
@@ -22,6 +58,8 @@ describe UserSessionsController do
           flash[:alert].should be_nil
         end
 
+        its(:current_user) { should eq current_user }
+
       end
 
       describe 'when alternative superuser has not been configured' do
@@ -31,9 +69,12 @@ describe UserSessionsController do
 
         it { should redirect_to(root_path) }
 
-        it 'should display no alerts' do
-          flash[:alert].should be_nil
+        describe 'flash alert' do
+          subject { flash[:alert] }
+          it { should eq 'MeritBadges is not initialized.  Please login as superuser and configure another user as superuser.' }
         end
+
+        its(:current_user) { should be_nil }
       end
     end
   end
