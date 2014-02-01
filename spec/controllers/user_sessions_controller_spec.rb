99bc315227ad42d23e38997300247c11a9fd42ca
tcox5698 <tcox56_98@yahoo.com>
2014-02-01 09:22:38 -0600

    gonna invalidate rubymine caches

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -3,21 +3,37 @@ require 'spec_helper'
 describe UserSessionsController do
   describe 'POST #create' do
     describe 'when logging in as normal user' do
-      describe 'and alternative superuser has been already been configured' do
+      let(:current_user) { create :user, email: 'newguy@here.com' }
+
+      before do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+      end
+
+      describe 'when alternative superuser has been already been configured' do
         before do
-          current_user = create :user, email: 'newguy@here.com'
           alt_super = create :user
           alt_super.roles << Role.find_by_name('superuser')
-          @request.env["devise.mapping"] = Devise.mappings[:user]
           post :create, user: {email: current_user.email, password: 'factory!'}
         end
 
-        it {should redirect_to(root_path)}
+        it { should redirect_to(root_path) }
 
         it 'should display no alerts' do
           flash[:alert].should be_nil
         end
 
+      end
+
+      describe 'when alternative superuser has not been configured' do
+        before do
+          post :create, user: {email: current_user.email, password: 'factory!'}
+        end
+
+        it { should redirect_to(root_path) }
+
+        it 'should display no alerts' do
+          flash[:alert].should be_nil
+        end
       end
     end
   end
