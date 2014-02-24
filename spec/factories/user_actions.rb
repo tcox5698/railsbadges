c2662837637792ee1e6159a3223cc9b4faf8ca05
tcox5698 <tcox56_98@yahoo.com>
2014-02-23 22:25:20 -0600

    still cleaning up that test

--- spec/factories/user_actions.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :user_action do
-    name "MyString"
-    action_date "2014-02-09 19:16:56"
-    user nil
-  end
-end
